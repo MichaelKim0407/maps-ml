76c8f1e267b8b8bf40031ad57d057f5afc9e375c
Michael Kim <mkim0407@gmail.com>
2018-03-18 15:48:30 -0700

    [FEATURE] download data

---
+++ download.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+import location
+
+__author__ = 'Michael'
+
+
+def random_points(n):
+    for p in np.random.random((n, 2)):
+        yield list(p)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('output')
+    parser.add_argument('count', type=int)
+    args = parser.parse_args()
+
+    with open(args.output, 'w') as f:
+        for p in random_points(args.count):
+            l = location.Location(*p)
+            f.write(str(l))
+            f.write('\n')
+            f.flush()
